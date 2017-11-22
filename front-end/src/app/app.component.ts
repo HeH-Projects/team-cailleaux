054e9e9a83f1eace69e5633bdfa129a722105903
Julien <julmougenot@gmail.com>
2017-11-22 12:14:06 +0100

    Aesthetics
    Now there's a bar under the current page's name

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -7,8 +7,24 @@ declare var $: any;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Vet Manager';
+  agenda  = true;
 
-  
+  nav(page: string): void {
+    if(page == 'agenda' && !this.agenda){
+      this.agenda = true;
+    }
+    else if(page == 'gestion' && this.agenda){
+      this.agenda = false;
+    }
+  }
+
+  current = 'clients';
+
+  setCurrent(page: string) {
+    if (this.current == page) { return '#002288' }
+    else { return 'transparent'; }
+  }
 }
